5605491fde8fcb30dacec79c42df7b16147a6c85
Matthew Strait <strait@physics.umn.edu>
2016-12-28 16:32:01 -0600

    minor refactoring to be sane

--- postmuon_module.cc
+++ postmuon_module.cc
@@ -26,6 +26,16 @@
 
 #include "art/Framework/Core/EDAnalyzer.h"
 
+struct pm{
+  int cluster_nhit;
+  int last_accepted_time;
+  int last_accepted_i;
+  float dist2sum; // summed hit distance from track end
+  int asum;       // sumed ADC of a cluster of delayed hits
+  float esum;     // summed calibrated energy, in MeV
+  int cluster_i;
+};
+
 
 /// Calibrating RawData to Produce CellHits
 namespace PostMuon {
@@ -71,21 +81,21 @@ static float dist_trackend_to_cell(const rb::Track & trk,
 
   const bool increasing_z = claimed_increasing_z ^ flip;
 
-  const int lastplane = increasing_z?std::max(lastplane_even, lastplane_odd) 
+  const int lastplane = increasing_z?std::max(lastplane_even, lastplane_odd)
                                     :std::min(lastplane_even, lastplane_odd);
 
   return
     chit.Plane()%2 == 0 ?
-      sqrt(pow(planes_per_cell*(chit.Plane() - lastplane)   , 2) + 
+      sqrt(pow(planes_per_cell*(chit.Plane() - lastplane)   , 2) +
            pow(                 chit.Cell()  - lastcell_even, 2))
-   :  sqrt(pow(planes_per_cell*(chit.Plane() - lastplane)   , 2) + 
+   :  sqrt(pow(planes_per_cell*(chit.Plane() - lastplane)   , 2) +
            pow(                 chit.Cell()  - lastcell_odd , 2));
 }
 
 /*
  Return the average hit time of the latest 40 hits (or all of them).
 
- This is meant to provide a robust measure of the track time, even if 
+ This is meant to provide a robust measure of the track time, even if
  an early Michel decay gets reconstructed as part of the track.
 */
 static int mean_late_track_time(const rb::Track & trk)
@@ -102,7 +112,7 @@ static int mean_late_track_time(const rb::Track & trk)
   for(unsigned int i = std::max(0, (int)tdcs.size() - max_for_avg); i < tdcs.size(); i++)
     acc += tdcs[i];
 
-  return int(acc / std::min(max_for_avg, (int)tdcs.size()) + 0.5); 
+  return int(acc / std::min(max_for_avg, (int)tdcs.size()) + 0.5);
 }
 
 /*
@@ -110,7 +120,7 @@ static int mean_late_track_time(const rb::Track & trk)
  in cells (where a plane is ~2 cells).  Otherwise, return -1.
 */
 static double hit_near_track(const rb::Track & trk,
-  const int lasthiti_even, const int lasthiti_odd, 
+  const int lasthiti_even, const int lasthiti_odd,
   const art::Handle< std::vector<rb::CellHit> > & cellcol,
   const int celli)
 {
@@ -131,9 +141,6 @@ static double hit_near_track(const rb::Track & trk,
   const double dist = dist_trackend_to_cell(trk, chit, lasthiti_even,
                                             lasthiti_odd);
 
-
-  // Hits too close, but otherwise correct, probably signal that we
-  // are picking up a Michel decay.  Bail out.
   if(dist > maxdist_in_cells) return -1;
 
   return dist;
@@ -168,12 +175,8 @@ static void last_hits(int & lasthiti_even,
 static void print_ntuple_line(const art::Event & evt,
                               const rb::Track & trk,
                               const double eventlength,
-                              const double meandist2,
                               const int hittime_tdc,
-                              const int asum,  // XXX too many parameters...
-                              const float esum,
-                              const int cluster_i,
-                              const int cluster_nhit)
+                              const pm answer)
 {
   const int tracktime = mean_late_track_time(trk);
   const double timeleft = eventlength - tracktime *kUSEC_PER_TDC;
@@ -193,11 +196,12 @@ static void print_ntuple_line(const art::Event & evt,
                  "%f %f %f "
                  "%f %d %f %d %f\n",
     evt.run(), evt.event(),
-    cluster_i,
+    answer.cluster_i,
     (hittime_tdc - tracktime)*kUSEC_PER_TDC,
     tsx, tsy, tsz,
     tx, ty, tz,
-    meandist2, asum, esum, cluster_nhit, timeleft);
+    answer.dist2sum/answer.cluster_nhit,
+    answer.asum, answer.esum, answer.cluster_nhit, timeleft);
 }
 
 namespace PostMuon{
@@ -206,7 +210,7 @@ PostMuon::PostMuon(fhicl::ParameterSet const& pset)
   : EDAnalyzer(pset), fRemoveBadChans(pset.get<bool>("RemoveBadChans")),
   fRawDataLabel(pset.get< std::string >("RawDataLabel"))
 {
-  
+
 }
 
 static bool compare_cellhit(const rb::CellHit & a, const rb::CellHit & b)
@@ -215,6 +219,19 @@ static bool compare_cellhit(const rb::CellHit & a, const rb::CellHit & b)
 }
 
 PostMuon::~PostMuon() { }
+
+static pm mkpm()
+{
+  pm res;
+  res.cluster_nhit = 0;
+  res.last_accepted_time = -1;
+  res.last_accepted_i = -1;
+  res.dist2sum = 0;
+  res.asum = 0;
+  res.esum = 0;
+  res.cluster_i = 0;
+  return res;
+}
 
 void PostMuon::analyze(const art::Event& evt)
 {
@@ -240,9 +257,10 @@ void PostMuon::analyze(const art::Event& evt)
 
   art::ServiceHandle<calib::Calibrator> calthing;
 
-  if((*rawtrigger).empty()) return;
+  if(rawtrigger->empty()) return;
 
-  const double triggerlength = (*rawtrigger)[0].fTriggerRange_TriggerLength*kUSEC_PER_MICROSLICE;
+  const double triggerlength = (*rawtrigger)[0].
+    fTriggerRange_TriggerLength*kUSEC_PER_MICROSLICE;
 
   for(unsigned int t = 0; t < tracks->size(); t++){
 
@@ -250,15 +268,7 @@ void PostMuon::analyze(const art::Event& evt)
     int lasthiti_even = 0, lasthiti_odd = 0;
     last_hits(lasthiti_even, lasthiti_odd, trk);
 
-    // XXX needs to be refactored to not have a pile of variables
-    // XXX that have to be cleared periodically.
-    int cluster_nhit = 0;
-    int last_accepted_time = -1;
-    int last_accepted_i = -1;
-    float dist2sum = 0; // summed hit distance from track end
-    int asum = 0; // sumed ADC of a cluster of delayed hits
-    float esum = 0; // summed calibrated energy, in MeV
-    int cluster_i = 0;
+    pm answer = mkpm();
 
     // CellHits do *not* come in time order
     std::vector<rb::CellHit> sorted_hits;
@@ -274,7 +284,7 @@ void PostMuon::analyze(const art::Event& evt)
 
       const double dist2 = dist*dist;
 
-      dist2sum += dist2;
+      answer.dist2sum += dist2;
 
       const rb::CellHit & chit = (*cellcol)[c];
 
@@ -283,43 +293,34 @@ void PostMuon::analyze(const art::Event& evt)
          chit.Plane()%2 == 0? (flip?trk.Start().X():trk.Stop().X()): // doc-11570
                               (flip?trk.Start().Y():trk.Stop().Y()));
 
-      // Hits are time ordered.  Only report on hits that are
-      // separated in time from other accepted hits by at least
-      // one quiet TDC tick.
-      const bool newcluster = chit.TDC() > last_accepted_time + 1;
+      // Hits are time ordered.  Only report on hits that are separated
+      // in time from other accepted hits by at least 1 quiet TDC tick.
+      const bool newcluster = chit.TDC() > answer.last_accepted_time + 1;
 
-      if(newcluster && cluster_nhit){
-        cluster_nhit++;
+      if(newcluster && answer.cluster_nhit){
+        answer.cluster_nhit++;
         print_ntuple_line(evt, trk, triggerlength,
-                          dist2sum/cluster_nhit, chit.TDC(),
-                          asum, esum, cluster_i++, cluster_nhit);
-        asum = esum = cluster_nhit = dist2sum = 0;
+                          chit.TDC(), answer);
+        answer.cluster_i++;
+        answer.asum = answer.esum = answer.cluster_nhit = answer.dist2sum = 0;
       }
       else{
-        cluster_nhit++;
-      }
-     
-
-      asum += chit.ADC();
-      if(rhit.IsCalibrated()){
-        esum += rhit.GeV()*1000;
-      }
-      else{
-        // For now, just throw away uncalibrated hits,
-        // XXX is there a better approach?
-        //printf("Uncalibrated ADC: %d\n", chit.ADC());
+        answer.cluster_nhit++;
       }
 
-      last_accepted_time = chit.TDC();
-      last_accepted_i = c;
+      answer.asum += chit.ADC();
+      // For now, just throw away uncalibrated hits,
+      // XXX is there a better approach?
+      if(rhit.IsCalibrated()) answer.esum += rhit.GeV()*1000;
+
+      answer.last_accepted_time = chit.TDC();
+      answer.last_accepted_i = c;
     }
 
     // print last cluster
-    if(cluster_nhit)
+    if(answer.cluster_nhit)
       print_ntuple_line(evt, trk, triggerlength,
-                        dist2sum/cluster_nhit,
-                        (*cellcol)[last_accepted_i].TDC(),
-                        asum, esum, cluster_i++, cluster_nhit);
+                        (*cellcol)[answer.last_accepted_i].TDC(), answer);
 
   }
 }
